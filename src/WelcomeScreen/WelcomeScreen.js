db8cacf2cf5554ee244c7636d18fdf38950361a9
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-03-02 11:43:05 -0700

    Add baseball logo to welcome screen and favicon

--- src/WelcomeScreen/WelcomeScreen.js
+++ src/WelcomeScreen/WelcomeScreen.js
@@ -6,6 +6,7 @@ export const WelcomeScreen = () => {
   console.log('welcome');
   return (
     <section className="welcome-screen">
+      <img src={process.env.PUBLIC_URL + '/baseball-logo.png'} />
       <h3>Welcome to</h3>
       <h1>Just Baseball.</h1>
       <Link className="enter-btn" to='/games'>ENTER</Link>
