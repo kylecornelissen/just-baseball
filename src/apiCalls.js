e4926062f22f762a6a00a87bd88080846d04c1b6
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-03-01 19:01:10 -0700

    Add get highlights fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,5 @@
 const baseURL = 'https://statsapi.mlb.com/api/v1/';
 
-
 // example: /schedule?sportId=1&startDate=07/01/2018&endDate=07/01/2018
 
 export const getGames = async (date) => {
@@ -11,4 +10,15 @@ export const getGames = async (date) => {
   }
   const games = await res.json();
   return games.dates[0].games;
+}
+
+// example: /game/530661/content
+
+export const getHighlights = async (gameID) => {
+  const res = await fetch(`${baseURL}game/${gameID}/content`)
+  if (!res.ok) {
+    throw new Error('Invalid.');
+  }
+  const highlights = await res.json();
+  return highlights.highlights.live.items;
 }
