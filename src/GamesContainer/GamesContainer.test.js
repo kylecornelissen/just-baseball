486bbc17e82297284b8f9ff6350e704f5cec1a4b
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-03-02 14:04:22 -0700

    Add mapState and mapDipatch redux tests for Games Container

---
+++ src/GamesContainer/GamesContainer.test.js
@@ -0,0 +1,54 @@
+import { GamesContainer, mapStateToProps, mapDispatchToProps } from './GamesContainer';
+import {setGames} from '../actions/actions';
+
+describe('GamesContainer', () => {
+  const games = [
+    {
+      id: 1,
+      awayTeam: "Atlanta Barves",
+      awayScore: 0,
+      homeTeam: "Washington Nationals",
+      homeScore: 42
+    },
+    {
+      id: 2,
+      awayTeam: "New York Yankees",
+      awayScore: 2,
+      homeTeam: "Boston Red Sox",
+      homeScore: 3
+    }
+  ]
+
+  describe('mapStateToProps', () => {
+    it('should return an object with the games array', () => {
+      // SETUP
+      const mockState = {
+        games: games
+      };
+      const expected = {
+        games: games
+      };
+
+      // EXECUTION
+      const mappedProps = mapStateToProps(mockState);
+
+      // EXPECTATION
+      expect(mappedProps).toEqual(expected);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with the setGames action when setGames is called', () => {
+      // SETUP
+      const mockDispatch = jest.fn();
+      const actionToDispatch = setGames(games);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+
+      // EXECUTION
+      mappedProps.setGames(games);
+
+      // EXPECTATION
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+  });
+});
