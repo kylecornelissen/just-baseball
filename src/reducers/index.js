52aed6e74f32f748dba52208074c80e5eb5c449b
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-03-01 19:01:31 -0700

    Add set highlights reducer and actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { gamesReducer } from './gamesReducer';
+import { highlightsReducer } from './highlightsReducer';
 
 export const rootReducer = combineReducers({
-  games: gamesReducer
+  games: gamesReducer,
+  highlights: highlightsReducer
 });
