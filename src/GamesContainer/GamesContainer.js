f2fd51c37dba0c528fdabfef0f1a612af163be7d
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-03-04 15:03:23 -0700

    Add datepicker with ability to handleChange and change dates of the games in games container

--- src/GamesContainer/GamesContainer.js
+++ src/GamesContainer/GamesContainer.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 import './GamesContainer.scss';
 import GameCard from '../GameCard/GameCard';
 import {getGames} from '../apiCalls';
-import {setGames} from '../actions/actions';
+import {setGames, setDate} from '../actions/actions';
 
 class GamesContainer extends Component {
   async componentDidMount() {
-    const date = this.formatDate(new Date());
+    const { date } = this.props;
+    const newDate = await date || this.formatDate(new Date());
+    await this.loadGames(newDate);
+  }
+  loadGames = async (date) => {
     try {
+      this.props.setDate(date);
       let games = await getGames(date);
       games = this.filterGames(games.dates[0].games);
       this.props.setGames(games);
@@ -18,11 +25,12 @@ class GamesContainer extends Component {
     }
   }
   formatDate = (date) => {
-    console.log(`${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`);
-    return `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    const month = `${String(date.getMonth()+1).length < 2 ? 0 : ''}${date.getMonth()+1}`;
+    const day = `${String(date.getDate()).length < 2 ? 0 : ''}${date.getDate()}`;
+    return {month, day, year: date.getFullYear(), monthABC: months[month-1]}
   }
   filterGames = (games) => {
-    console.log(games);
     return games.map(game => {
       return {
         id: game.gamePk,
@@ -33,27 +41,44 @@ class GamesContainer extends Component {
       }
     })
   }
+  handleChange = async (date) => {
+    console.log(date);
+    date = await this.formatDate(date);
+    this.loadGames(date);
+  }
   render() {
-    const { games } = this.props;
+    const { games, date } = this.props;
     const gameCards = games.map(game => {
       return <GameCard key={game.gamePk} game={game} />
     });
 
     return (
       <section className="games-container">
-        <h2>This Date's Games</h2>
-        <div>{gameCards}</div>
+        <div className="calendar-date-header">
+          <label className="date-selection-label">
+            <h2>{date.monthABC} {parseInt(date.day)}, {date.year}</h2>
+            <DatePicker
+              className="game-date-picker"
+              selected={new Date()}
+              onSelect={this.handleChange}
+            />
+            <img src={process.env.PUBLIC_URL + '/calendar.png'} alt="calendar" />
+          </label>
+        </div>
+        <div className="game-cards-container">{gameCards ? gameCards : <span>No games on this day.</span>}</div>
       </section>
     )
   }
 }
 
 export const mapStateToProps = state => ({
-  games: state.games
+  games: state.games,
+  date: state.date
 });
 
 export const mapDispatchToProps = dispatch => ({
-  setGames: games => { dispatch(setGames(games)) }
+  setGames: games => { dispatch(setGames(games)) },
+  setDate: date => { dispatch(setDate(date)) }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(GamesContainer);
